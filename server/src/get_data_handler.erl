c0ebf798554b73f7d21111f2dde63803c09d3a4c
Eric S. Londres <elondres@stevens.edu>
2021-05-18 12:38:59 -0400

    connect to redis

--- server/src/get_data_handler.erl
+++ server/src/get_data_handler.erl
@@ -3,6 +3,10 @@
 -export([init/2]).
 
 init(Req, State) ->
-    % TODO: Get info from Redis cache
-    % TODO: Return that info
+    Token = cowboy_req:binding(token, Req),	% Parse the requested token
+    io:fwrite("~s~n", [Token]),			% for debug purposes
+    whereis(cache)!{get, self(), binary_to_list(Token)}, % Get the stat info from the cache
+    receive {ok, Stats} ->
+	    Req = cowboy_req:reply(200, #{<<"content-type">> => <<"application/json">>}, Stats, Req) % Return the info
+    end,
     {ok, Req, State}.
--- server/src/server_app.erl
+++ server/src/server_app.erl
@@ -5,6 +5,7 @@
 -export([stop/1]).
 
 start(_Type, _Args) ->
+    register(cache, spawn(cache, start, [])),
     Dispatch = cowboy_router:compile([{'_', [{"/", data_handler, []},
 					     {"/:token", get_data_handler, []}]}]),
     {ok, _} = cowboy:start_clear(auth_handler, [{port, 8080}], #{env => #{dispatch => Dispatch}}),
